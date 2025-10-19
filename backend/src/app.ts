22f76b4266d10fa0039cf0cabb7c2a75cfae960f
Leshego Talane <repo.talane@bbd.co.za>
2025-10-19 20:52:21 +0200

    added to routes clientInfoMiddleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import swaggerUi from 'swagger-ui-express';
 import { swaggerDocument } from './swagger';
 
 import truckRoutes from './routes/truckRoutes';
@@ -14,27 +13,24 @@ import simulationRoutes from './routes/simulationRoutes';
 
 
 import { errorMiddleware } from './middleware/errorMiddleware';
-//import { certInfoMiddleware } from './middleware/certMiddleware';
+import { clientInfoMiddleware } from './middleware/certMiddleware';
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
-
-// Swagger Documentation
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(clientInfoMiddleware);
 
 app.use('/api/companies', companyRoutes);
 app.use('/api/trucks', truckRoutes);
-app.use('/api/pickups', pickupRoutes);
+app.use('/api/pickups',  pickupRoutes);
 app.use('/api/webhook', webhookRoutes);
 app.use('/api/simulation', simulationRoutes); 
 app.use('/api/analytics', analyticsRoutes);
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok', message: 'Service is healthy', clientName: (req as any).clientName || 'unknown' });
+  res.status(200).json({ status: 'ok', message: 'Service is healthy', clientId: (req as any).clientId || 'unknown' });
 });
 
 app.use(errorMiddleware);
